c1a7854ab87dde76d7b06f55cdd4214464c262f9
Matthew Maguire <matpmag@gmail.com>
2014-12-08 17:37:06 +0000

    Wackamole
    Added first bonus level concept.

---
+++ Exodus Challenge/Exodus Challenge/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Exodus_Challenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxMole = new System.Windows.Forms.PictureBox();
+            this.moleTicker = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMole)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxMole
+            // 
+            this.pbxMole.Image = global::Exodus_Challenge.Properties.Resources.avatar;
+            this.pbxMole.Location = new System.Drawing.Point(12, 12);
+            this.pbxMole.Name = "pbxMole";
+            this.pbxMole.Size = new System.Drawing.Size(95, 91);
+            this.pbxMole.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxMole.TabIndex = 0;
+            this.pbxMole.TabStop = false;
+            this.pbxMole.Click += new System.EventHandler(this.pbxMole_Click);
+            this.pbxMole.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbxMole_MouseDown);
+            // 
+            // moleTicker
+            // 
+            this.moleTicker.Enabled = true;
+            this.moleTicker.Interval = 700;
+            this.moleTicker.Tick += new System.EventHandler(this.moleTicker_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 697);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbxMole);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxMole)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxMole;
+        private System.Windows.Forms.Timer moleTicker;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Exodus Challenge/Exodus Challenge/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exodus_Challenge
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+
+
+        private void moleTicker_Tick(object sender, EventArgs e)
+        {
+            int maxLeft = this.Width - pbxMole.Width - 20;
+            int maxTop = this.Height - pbxMole.Height - 20;
+            pbxMole.Left = random.Next(20, maxLeft);
+            pbxMole.Top = random.Next(20, maxTop);
+        }
+
+        private void pbxMole_Click(object sender, EventArgs e) // redundant
+        {
+            count++;
+            label1.Text = count.ToString();
+        }
+
+        private void pbxMole_MouseDown(object sender, MouseEventArgs e)
+        {
+            count+=10;
+            label1.Text = count.ToString();
+        }
+    }
+}
