b034a19bd20f910364ad4a09decdf884594d0b3d
Matthew Maguire <matpmag@gmail.com>
2015-01-05 00:50:08 +0000

    Code cleanup
    Codemaid cleanup.

--- Exodus Challenge/Exodus Challenge/Zone 2/SnakeSection.cs
+++ Exodus Challenge/Exodus Challenge/Zone 2/SnakeSection.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
-using System.Drawing;
-using System.IO;
 
 namespace Exodus_Challenge
 {
@@ -19,42 +13,17 @@ namespace Exodus_Challenge
 
     public struct SnakeGameInfo
     {
+        #region Fields
+
         public static Keys lastKey = Keys.Right;
-    }
-    public abstract class SnakeSection
-    {
-        public Color color = Color.Red;
-        public string imagepath;
-        //TODO: get imagepath string from folder
-        public Image image;
-        public int X { get; set; }
-        public int Y { get; set;}
-        public int LastX { get; set; }
-        public int LastY { get; set; }
-        public SnakeSection previous;
-        public Direction WillMove;
-        public Direction LastMove;
 
-        public abstract Direction NextMove { get; }
-
-        public abstract void SnakeSetup();
-
-        public SnakeSection()
-        {
-            SnakeSetup();
-        }
+        #endregion Fields
     }
 
     public class SnakeBody : SnakeSection
     {
-        public override void SnakeSetup()
-        {
-            X = 0;
-            Y = 0;
-            LastX = X;
-            LastY = Y;
-            //image = Image.FromFile();
-        }
+        #region Properties
+
         public override Direction NextMove
         {
             get
@@ -64,19 +33,27 @@ namespace Exodus_Challenge
                 return this.WillMove;
             }
         }
-    }
 
-    public class SnakeHead : SnakeSection
-    {
+        #endregion Properties
+
+        #region Methods
+
         public override void SnakeSetup()
         {
             X = 0;
             Y = 0;
             LastX = X;
             LastY = Y;
-            previous = this;
-            color = Color.Red;
+            //image = Image.FromFile();
         }
+
+        #endregion Methods
+    }
+
+    public class SnakeHead : SnakeSection
+    {
+        #region Properties
+
         public override Direction NextMove
         {
             get
@@ -87,17 +64,103 @@ namespace Exodus_Challenge
                     case Keys.Up:
                         this.WillMove = Direction.up;
                         return this.WillMove;
+
                     case Keys.Down:
                         this.WillMove = Direction.down;
                         return this.WillMove;
+
                     case Keys.Left:
                         this.WillMove = Direction.left;
                         return this.WillMove;
+
                     default:
                         this.WillMove = Direction.right;
                         return this.WillMove;
                 }
             }
         }
+
+        #endregion Properties
+
+        #region Methods
+
+        public override void SnakeSetup()
+        {
+            X = 0;
+            Y = 0;
+            LastX = X;
+            LastY = Y;
+            previous = this;
+            color = Color.Red;
+        }
+
+        #endregion Methods
+    }
+
+    public abstract class SnakeSection
+    {
+        #region Fields
+
+        public Color color = Color.Red;
+
+        //TODO: get imagepath string from folder
+        public Image image;
+
+        public string imagepath;
+        public Direction LastMove;
+
+        public SnakeSection previous;
+
+        public Direction WillMove;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public SnakeSection()
+        {
+            SnakeSetup();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public int LastX
+        {
+            get;
+            set;
+        }
+
+        public int LastY
+        {
+            get;
+            set;
+        }
+
+        public abstract Direction NextMove
+        {
+            get;
+        }
+
+        public int X
+        {
+            get;
+            set;
+        }
+
+        public int Y
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public abstract void SnakeSetup();
+
+        #endregion Methods
     }
 }
